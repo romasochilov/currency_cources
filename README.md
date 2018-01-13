00843c25b18f891f39f7710a9e31a12e785bbf3f
Roman <romasochilov@gmail.com>
2018-01-13 10:00:00 +0300

    readme.md changed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # currency cources
 Django project , which allow watch currency cources from cryptonator.com
+Use url http:/127.0.0.1/crs to watch cources. 
--- currency_cources/urls.py
+++ currency_cources/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url('^', include('cources.urls')),
+    url('', include('cources.urls')),
     url('admin/', admin.site.urls),
 ]
